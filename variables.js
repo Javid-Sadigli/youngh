6f18a92fec3fb22181ee79951c4b4ed269ff681c
Javid Sadigli <cavidoffical@gmail.com>
2024-06-11 17:38:21 +0400

    Start again

--- variables.js
+++ variables.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = {
     DATABASE_URI : 'mongodb+srv://theyoungh:NPY4PLn20iopOH8w@cluster0.z3bge2n.mongodb.net/youngh?retryWrites=true&w=majority', 
     hostname : 'localhost', 
-    port : 3000, 
+    port : 5000, 
     mainDir : path.dirname(require.main.filename)
 };
