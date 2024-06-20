9b6e2f5b471aaa3ffb821579f59f401fb5516f36
Javid Sadigli <cavidoffical@gmail.com>
2024-06-20 19:15:04 +0400

    Fixed bug

--- backend/models/user.js
+++ backend/models/user.js
@@ -16,7 +16,7 @@ const UserSchema = new Schema({
         required : true
     },
     description : {
-        type : Text
+        type : String
     }, 
     userType : {
         type : String, 
