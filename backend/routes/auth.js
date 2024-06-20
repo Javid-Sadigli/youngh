78f5ec78e559a7a19def76d8f44a0565baaefd63
Javid Sadigli <cavidoffical@gmail.com>
2024-06-20 19:13:05 +0400

    Added controllers to the routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,5 +4,9 @@ const router = express.Router();
 // Controllers 
 const authController = require('../controllers/auth');
 
+/* Start handling */ 
+router.post('/register', authController.POST_Register); 
+router.post('/login', authController.POST_Login); 
+/* End handling */ 
 
 module.exports = router;
